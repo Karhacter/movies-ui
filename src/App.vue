18ac6b97b95d977c094ee268f20c34b4af75c50e
Karhacter <109360401+Karhacter@users.noreply.github.com>
2025-03-16 18:24:50 +0700

    add a little changes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,15 @@
 <template>
-  <v-app>
-    <component :is="layout" />
-  </v-app>
+  <div>
+    <component :is="layout"> </component>
+  </div>
 </template>
 
 <script>
-  import './assets/css/style.css'
-  import './assets/css/bootstrap.min.css'
-  import './assets/css/style-min.css'
-  import './assets/css/cards.css'
-  import './assets/css/faq.css'
-
 const default_layout = 'default'
 
 export default {
   name: 'App',
   components: {},
-
   computed: {
     layout() {
       return (this.$route.meta.layout || default_layout) + '-layout'
---
+++ src/components/Category.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dropdown-content" style="color: white">
+    <div v-for="category in categories" :key="category.id">
+      <router-link :to="'/' + category.link" class="category-link">
+        {{ category.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    categories: [],
+  }),
+
+  mounted() {
+    fetch('http://localhost:9000/api/categories/index')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('API Response:', data) // Debug API response
+        this.categories = data // Correctly assign data
+      })
+      .catch((error) => console.error('Error fetching categories:', error))
+  },
+}
+</script>
+
+<style scoped>
+.category-link {
+  text-decoration: none;
+  color: white;
+  padding: 8px;
+  display: block;
+}
+</style>
